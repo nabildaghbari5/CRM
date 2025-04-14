1bd94f34b1a4aca37ede40a7eceda6944093a50f
nabil daghbari <n.daghbari@bewisetech.fr>
2025-04-14 11:24:38 +0100

    CRM sécurity

---
+++ Backend/src/main/java/com/crm/model/Role.java
@@ -0,0 +1,13 @@
+package com.crm.model;
+
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role {
+
+    ADMINISTRATEUR ,
+    User ,
+
+
+}
---
+++ Backend/src/main/java/com/crm/service/UserService.java
@@ -0,0 +1,15 @@
+package com.crm.service;
+
+import com.crm.model.User;
+
+import java.security.Principal;
+
+
+
+public interface UserService extends BaseService<User, Integer>  {
+
+    User accepterOrRefuserAccount(Integer idUser, int etat);
+    String validateToken(String token);
+    void createPasswordResetTokenForUser(User user, String passwordToken);
+    void saveUserVerificationToken(User theUser, String verificationToken);
+}
